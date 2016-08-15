027e179d418e4fc00285fdbc11ac8376adcba113
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-08-15 16:43:35 -0400

    add block on click

--- app/assets/javascripts/collections.js
+++ app/assets/javascripts/collections.js
@@ -4,7 +4,9 @@ var CollectionsController = Paloma.controller('Collections', {
 		var blockOptions = {
 			container: 'grid'
 		}
-		new App.addBlock(blockOptions);
+		$('[data-trigger=new-block]').on('click', function(){
+			new App.addBlock(blockOptions);
+		})
 
 	}, 
 	index: function(){
@@ -21,5 +23,5 @@ window.App.addBlock = function(options){
 }
 
 App.createBlock = function(){
-	return '<h2>block</h2>'
+	return '<div class="block-select"><span>block 1</span><span>block 2</span><span>block 3</span></div>'
 }
